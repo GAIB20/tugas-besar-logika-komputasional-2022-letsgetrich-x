904cc1eadf4cd8c902399deca9ce6d2c44847291
Alexander Jason <alexanderjason526@gmail.com>
2022-11-15 00:53:36 +0700

    add properties

---
+++ src/f03_property.pl
@@ -0,0 +1,57 @@
+/* hargaBeliProp(loc, jenis, hargaBeli)*/
+/*
+    tanah = tanah
+    bg1 = bangunan 1
+    bg2 = bangunan 2
+    bg3 = bangunan 3
+    lm = landmark
+*/
+hargaBeli(a1, tanah, 200).
+hargaBeli(a1, bg1, 1000).
+hargaBeli(a1, bg2, 2000).
+hargaBeli(a1, bg3, 3000).
+hargaBeli(a1, lm, 3000).
+hargaBeli(a2, tanah, 150).
+hargaBeli(a2, bg1, 1050).
+hargaBeli(a2, bg2, 2000).
+hargaBeli(a2, bg3, 3000).
+hargaBeli(a2, lm, 2950).
+hargaBeli(a3, tanah, 100).
+hargaBeli(a3, bg1, 1000).
+hargaBeli(a3, bg2, 2100).
+hargaBeli(a3, bg3, 3000).
+hargaBeli(a3, lm, 2900).
+
+hargaBeli(b1, tanah, 300).
+hargaBeli(b1, bg1, 1200).
+hargaBeli(b1, bg2, 2200).
+hargaBeli(b1, bg3, 3200).
+hargaBeli(b1, lm, 3200).
+hargaBeli(b2, tanah, 300).
+hargaBeli(b2, bg1, 1250).
+hargaBeli(b2, bg2, 2100).
+hargaBeli(b2, bg3, 3300).
+hargaBeli(b2, lm, 3200).
+hargaBeli(b3, tanah, 350).
+hargaBeli(b3, bg1, 1200).
+hargaBeli(b3, bg2, 2200).
+hargaBeli(b3, bg3, 3200).
+hargaBeli(b3, lm, 3200).
+
+hargaBeli(c1, tanah, 400).
+hargaBeli(c1, bg1, 1500).
+hargaBeli(c1, bg2, 2500).
+hargaBeli(c1, bg3, 3200).
+hargaBeli(c1, lm, 3500).
+hargaBeli(c2, tanah, 400).
+hargaBeli(c2, bg1, 1450).
+hargaBeli(c2, bg2, 2100).
+hargaBeli(c2, bg3, 3300).
+hargaBeli(c2, lm, 3500).
+hargaBeli(c3, tanah, 450).
+hargaBeli(c3, bg1, 1400).
+hargaBeli(c3, bg2, 2500).
+hargaBeli(c3, bg3, 3600).
+hargaBeli(c3, lm, 4000).
+
+

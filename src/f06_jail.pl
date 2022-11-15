6f260267ee8ec33c766ae7f00f5d1d87f9bd85b5
chaerla <chelgchen@gmail.com>
2022-11-15 20:47:53 +0800

    add jail mechanism and fixed some features in jail

--- src/f06_jail.pl
+++ src/f06_jail.pl
@@ -1,21 +1,19 @@
+:- include('f10_dice.pl').
 :- include('f09_player.pl').
-:- dynamic(in_jail/2).
+:- dynamic(in_jail/1).
 :- dynamic(dice_after_jail/2).
 
 /* inJail(ID, state), state : 0 false, 1 true */
 initJail:-
-    asserta(in_jail(1,0)),
-    asserta(in_jail(2,0)),
     asserta(dice_after_jail(1,0)),
     asserta(dice_after_jail(2,0)).
 
 jailMechanism:-
     currentPlayer(X),
     dice_after_jail(X, 0),
-    playerDouble(3),
-    retractall(in_jail(X,Y)),
-    asserta(in_jail(X,1)),
-    write('Ooops, Anda masuk ke Azkaban!\n'),
+    playerDouble(X,3),
+    asserta(in_jail(X)),
+    write('Ooops! What a bad muggle you have been. Welcome to Azkaban!\n'),
     checkLocationDetail(jl),
     incDiceAfterJail,
     !.
@@ -23,24 +21,26 @@ jailMechanism:-
 jailMechanism:-
     currentPlayer(X),
     dice_after_jail(X,Y),
-    in_jail(X,1),
+    in_jail(X),
     Y<3,
     playerDouble(X,Z),
     (
         Z = 0 ->  incDiceAfterJail,
-                  write('Anda masih berada dalam Azkaban :(\n'),
+                  write('You\'re still in Azkaban :(\n'),
                   !;
         Z > 0 -> escapeJail,!
-        /* nunggu chance card */
     ),
     !.
 
 jailMechanism:-
     currentPlayer(X),
-    in_jail(X,1),
+    in_jail(X),
     dice_after_jail(X, 3),
     escapeJail,
     !.
+
+jailMechanism:-
+    write('hafhaskfskafjiasj').
 
 incDiceAfterJail:-
     currentPlayer(X),
@@ -51,8 +51,10 @@ incDiceAfterJail:-
     asserta(dice_after_jail(X,Z1)),
     !.
 
+visitJail:-
+    write('You\'ve arrived in Azkaban.. Normally, people lose their sanity here. I certainly hope you don\'nt, \n').
+
 escapeJail:-
-    write('Anda telah dilepaskan dari Azkaban. Silahkan melempar dadu!\n'),
+    write('Congratulations! You survived your time in Azkaban. You can throw dice now!\n'),
     currentPlayer(X),
-    retractall(in_jail(X, Y)),
-    asserta(in_jail(X,0)).
+    retractall(in_jail(X)).

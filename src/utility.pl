c37c1b1baa434fdb6c4577a9291f5a5071bd7316
Jeffrey Chow <16521353@mahasiswa.itb.ac.id>
2022-11-16 08:49:34 +0700

    feat: insertLast

--- src/utility.pl
+++ src/utility.pl
@@ -1,3 +1,8 @@
+/* insertLast */
+insertLast(Insert,[Head|[]],[Head,Insert]) :- !.
+insertLast(Insert,[Head|Tail],[Head|NewList]) :-
+    insertLast(Insert,Tail,NewList).
+
 /* deleteAtList, idx dimulai dari 1 */
 deleteAtList(1,[Head|Tail],Tail) :- !.
 deleteAtList(Idx,[Head|Tail],[Head|NewList]) :-

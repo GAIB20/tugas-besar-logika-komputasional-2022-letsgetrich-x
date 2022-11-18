e3379b50ec548bc3345aa01aaa47f3602a483b56
chaerla <chelgchen@gmail.com>
2022-11-19 01:34:56 +0800

    feat: added mechanism for go to azkaban card

--- src/f04_chance.pl
+++ src/f04_chance.pl
@@ -9,7 +9,7 @@ card_chance('Improper Use', 5).
 card_chance('Get Out From Azkaban', 3).
 card_chance('Quidditch Game', 1).
 card_chance('Knight Bus', 2). 
-card_chance('Gift', 2).
+card_chance('Gift', 3).
 
 
 card_info('Go To Azkaban', '__| |____________________________________________| |__\n(__   ____________________________________________   __)\n   | |                GO TO AZKABAN               | |\n   | |                                            | |\n   | |       You used an unforgiveable curse,     | |\n   | |          I hope you enjoy your time        | |\n   | |        with the Dementors in Azkaban..     | |\n __| |____________________________________________| |__\n(__   ____________________________________________   __)\n   | |                                            | | \n \n').
@@ -18,7 +18,7 @@ card_info('Tax', ' __| |____________________________________________| |__ \n(__ 
 card_info('Improper Use',' __| |____________________________________________| |__ \n(__   ____________________________________________   __)\n   | |               IMPROPER USE                 | |\n   | |                                            | |\n   | |     You\'ve been using magic without care   | |\n   | |      and caught the attention of Muggles!  | |\n   | |            Pay 200 as sanction.            | |\n __| |____________________________________________| |__\n(__   ____________________________________________   __)\n   | |                                            | | \n \n' ).
 card_info('Get Out From Azkaban',' __| |____________________________________________| |__ \n(__   ____________________________________________   __)\n   | |             GET OUT OF JAIL                | |\n   | |                                            | |\n   | |     Use this card to get out of Azkaban    | |\n   | |      in case, *just in case* you need to   | |\n   | |             serve time there..             | |\n __| |____________________________________________| |__\n(__   ____________________________________________   __)\n   | |                                            | | \n' ).
 card_info('Quidditch Game',' __| |____________________________________________| |__ \n(__   ____________________________________________   __)\n   | |               QUIDDITCH GAME               | |\n   | |                                            | |\n   | |       Any Wizard would enjoy some good     | |\n   | |     Quidditch Game, eh? Get 800 to watch   | |\n   | |           the game of the year!            | |\n __| |____________________________________________| |__\n(__   ____________________________________________   __)\n   | |                                            | | \n \n' ).
-card_ifo('Knight Bus',' __| |____________________________________________| |__ \n(__   ____________________________________________   __)\n   | |                  KNIGHT BUS                | |\n   | |                                            | |\n   | |       Get a chance to use Knight Bus!      | |\n   | |     Activate this card to use the knight   | |\n   | |         bus and go straight to GO          | |\n __| |____________________________________________| |__\n(__   ____________________________________________   __)\n  | |                                            | | \n \n' ).
+card_info('Knight Bus',' __| |____________________________________________| |__ \n(__   ____________________________________________   __)\n   | |                  KNIGHT BUS                | |\n   | |                                            | |\n   | |       Get a chance to use Knight Bus!      | |\n   | |     Activate this card to use the knight   | |\n   | |         bus and go straight to GO          | |\n __| |____________________________________________| |__\n(__   ____________________________________________   __)\n  | |                                            | | \n \n' ).
 card_info('Gift',' __| |____________________________________________| |__ \n(__   ____________________________________________   __)\n   | |                    GIFT                    | |\n   | |                                            | |\n   | |      Her name wouldn\'t be Mrs. Weasley     | |\n   | |     if she hadn\'t been so kind! You got    | |\n   | |       200 as a gift from Mrs. Weasley!     | |\n __| |____________________________________________| |__\n(__   ____________________________________________   __)\n   | |                                            | | \n \n' ).
 
 displayCards([]):-!.
@@ -27,8 +27,22 @@ displayCards([Head|Tail]):-
     write(Info),
     displayCards(Tail),
     !.
-
-goToAzkaban:-
+/* kurangin duit 1000 */
+cardMechanism('Attack By Death Eaters'):-!.
+/* pindahin ke pajak terDEKAT*/
+cardMechanism('Tax'):-!.
+/* kurangin duit 200 */
+cardMechanism('Improper Use'):-!.
+/* taro ke deck card player */
+cardMechanism('Get Out From Azkaban'):- escapeJail.
+/* tambahin duit 800 */
+cardMechanism('Quidditch Game'):-!.
+/* tambahin ke deck */
+cardMechanism('Knight Bus'):-!.
+/* tambahin duit 200 */
+cardMechanism('Gift'):-!.
+/* jeblosss */
+cardMechanism('Go To Azkaban'):-
     currentPlayer(X),
     retractall(playerDouble(X,_)),
     asserta(playerDouble(X,3)),
@@ -36,16 +50,26 @@ goToAzkaban:-
 
 useChanceCard:-
     chance_cards(List),
-    displayCards(List).
+    length(List, Length),
+    (
+        Length > 0 ->   write('These are your cards.'),
+                        displayCards(List),
+                        write('Which are you choosing, Wizard?'),
+                        read(Choice),
+                        cardMechanism(Choice);
+        write('You don\'t have any Chance Card yet.. :( \n')                
+    ),!.
 
 drawChanceCard:-
-    write('Welcome to Chance Card!'),
+    write('Welcome to Chance Card! Testing your luck without Felix Felicis eh?\n'),
     chance_cards(X),
     length(X, Length),
     random(1, Length, Rand),
     getValAtIdx(X, Rand, Card),
+    write('You drew a card!\n'),
     card_info(Card, Info),
-    write(Info).
+    write(Info),
+    cardMechanism(Card).
 
 
 insertCardToList(X, N):-

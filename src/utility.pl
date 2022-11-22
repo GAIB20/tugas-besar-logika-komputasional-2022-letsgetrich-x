2a121f1d21a05f7b124e72540193c661a4b055c9
Jeffrey Chow <16521353@mahasiswa.itb.ac.id>
2022-11-22 13:16:20 +0700

    fix: utility, singleton variables

--- src/utility.pl
+++ src/utility.pl
@@ -4,7 +4,7 @@ insertLast(Insert,[Head|Tail],[Head|NewList]) :-
     insertLast(Insert,Tail,NewList).
 
 /* deleteAtList, idx dimulai dari 1 */
-deleteAtList(1,[Head|Tail],Tail) :- !.
+deleteAtList(1,[_Head|Tail],Tail) :- !.
 deleteAtList(Idx,[Head|Tail],[Head|NewList]) :-
     NewIdx is Idx-1,
     deleteAtList(NewIdx,Tail,NewList).
@@ -12,7 +12,7 @@ deleteAtList(Idx,[Head|Tail],[Head|NewList]) :-
 
 /* getItemAtIdx */
 getItemAtIdx([Head|_], 1, Head) :- !.
-getItemAtIdx([Head|Tail], Index, Item) :-
+getItemAtIdx([_Head|Tail], Index, Item) :-
     NewIndex is Index - 1,
     getItemAtIdx(Tail, NewIndex, Item).
 
@@ -23,7 +23,7 @@ displayList([Head|Tail]):-
     write(Head),
     displayList(Tail).
 
-insertNTimes(Val, List, List, 0) :- !.
+insertNTimes(_Val, List, List, 0) :- !.
 insertNTimes(Val, List, Newlist, N) :-
     insertLast(Val, List, Inserted),
     N1 is N-1,
@@ -41,19 +41,19 @@ subset(List1,List2,Ans) :-
     AnsRec == 3 -> Ans is 1;
     Ans is 0.
 
-subsetRec([Head|Tail], List, Ans):- !.
+subsetRec([_Head|_Tail], _List, _Ans):- !.
 subsetRec([Head|Tail], List, Ans):-
     isIn(Head,List,In),
     subsetRec(Tail,List,NewAns),
     Ans is NewAns + In.
 
 /* isIn */
-isIn(X,[],0) :- !.
-isIn(X,[X|Tail],1) :-
-isIn(X,[Head|Tail],Ans) :-
+isIn(_X,[],0) :- !.
+isIn(X,[X|_Tail],1) :- !.
+isIn(X,[_Head|Tail],Ans) :-
     isIn(X,Tail,Ans).
 
-getIndex([], X, 0):-!.
+getIndex([], _X, 0):-!.
 getIndex([H|_], H, Index):-
     Index is 1, 
     !.

eee33bdd22c86d09136a8131aba47fe6f0ae3f10
Jeffrey Chow <16521353@mahasiswa.itb.ac.id>
2022-11-26 17:22:49 +0700

    feat: colorset scratch

--- src/b01_colorset.pl
+++ src/b01_colorset.pl
@@ -1,1 +1,19 @@
 /* Bonus 1 : Colorset */
+:-dynamic(list_colorset/1)
+
+colorset(a).
+colorset(b).
+colorset(c).
+colorset(d).
+colorset(e).
+colorset(f).
+colorset(g).
+colorset(h).
+
+colorsetMechanism :- !.
+
+/**
+
+
+
+**/

c9f25dd8b70501c71b92d6cca956994cc9d30023
Eugene316 <92687071+Eugene316@users.noreply.github.com>
2022-11-27 16:08:00 +0700

    chore: todo list for cheats

---
+++ src/cheats.pl
@@ -0,0 +1,5 @@
+/*  cheat 
+(misal memindahkan paksa pemain, 
+mengubah jumlah uang pemain, 
+mengubah kepemilikan dan level properti, 
+dan lain-lain untuk mencapai state yang diinginkan secara mudah dan cepat untuk kebutuhan demo) */

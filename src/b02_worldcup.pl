4d43d14ae1c02675bacc346c15bf35b7f22bcc26
Alexander Jason <alexanderjason526@gmail.com>
2022-11-28 21:16:46 +0700

    fix: world cup turns

--- src/b02_worldcup.pl
+++ src/b02_worldcup.pl
@@ -5,6 +5,8 @@ turnsAfterWorldCup(0).
 
 worldCupMechanism:-
     retractall(isWorldCup(_)),
+    retractall(turnsAfterWorldCup(_)),
+    asserta(turnsAfterWorldCup(0)),
     currentPlayer(X),
     write('Congratulations. You have been granted a chance to become Triwizard Tournament\'s host.'),nl,
     write('Triwizard Tournament is the largest magical contest held between the three largest wizarding schools of Europe'),nl,nl,
